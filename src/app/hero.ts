50ed0df3cc03f7f89b5f0fb3bfe73edf78b9b548
Rafi <rafi120396@gmail.com>
2019-04-02 15:09:51 +0530

    updates to deploy to heroku

--- src/app/hero.ts
+++ src/app/hero.ts
@@ -3,6 +3,7 @@ export class Hero {
     constructor(
         public id?: number,
         public uname?: string,
-        public credentialID?: string
+      public credentialID?: string,
+        public authenticators?: string[]
       ) {  }
 }
