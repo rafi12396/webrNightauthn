1b70fdc0710e82458c19689741f1e84885c485e2
Rafi <rafi120396@gmail.com>
2019-04-08 00:10:52 +0530

    updates to deploy to heroku

--- src/app/front/front.component.ts
+++ src/app/front/front.component.ts
@@ -97,7 +97,7 @@ this.navgtr.credentials.create({ 'publicKey': publicKey })
     .then((newCredentialInfo) => {
 
       let clientDataJSON = 'eyJjaGFsbGVuZ2UiOiJJSFdtWjFPa1MydDZLaHZYLWtvTnh1dGtZdU1WRXVuQ2pZTlNYWGdBeHZVIiwibmV3X2tleXNfbWF5X2JlX2FkZGVkX2hlcmUiOiJkbyBub3QgY29tcGFyZSBjbGllbnREYXRhSlNPTiBhZ2FpbnN0IGEgdGVtcGxhdGUuIFNlZSBodHRwczovL2dvby5nbC95YWJQZXgiLCJvcmlnaW4iOiJodHRwOi8vbG9jYWxob3N0OjMwMDAiLCJ0eXBlIjoid2ViYXV0aG4uY3JlYXRlIn0';
-      let clientData     = JSON.parse(base64url.decode(clientDataJSON));
+      let clientData     = JSON.parse(newCredentialInfo.response.clientDataJSON);
       console.log(clientData);
 
       // const cbor      = require('cbor');
